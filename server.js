f7c69ac666c17f7912044f5957c24dba0a174dc9
Guy Geva <guy1030@gmail.com>
2021-06-29 15:01:47 +0300

    server.js complete

--- server.js
+++ server.js
@@ -11,16 +11,66 @@ app.use(express.static('public'))
 app.use(cookieParser())
 app.use(express.json())
 
-// get Bugs list
+// Get Bugs list
 app.get('/api/bugs', (res, req) => {
   bugService.query()
     .then(bugs => res.send(bugs))
 })
 
-// get Single bug
+// Get Single bug
 app.get('/api/bugs/:bugId', (res, req) => {
   const { bugId } = req.params
   bugService.getById(bugId)
     .then(bug => res.send(bug))
+})
+
+// Create bug
+app.post('/api/bugs', (res, req) => {
+  const { _id, title, description, severity, createdAt, creaotr } = req.body
+  const bug = {
+    _id,
+    title,
+    description,
+    severity,
+    createdAt,
+    creator
+  }
+  bugService.save(bug)
+    .then(savedBug => {
+      console.log('savedBug', savedBug)
+      res.send(savedBug)
+    })
+})
+
+// Edit bug
+app.put('/api/bug', (res, req) => {
+  const { _id, title, description, severity, createdAt, creaotr } = req.body
+  const bug = {
+    _id,
+    title,
+    description,
+    severity,
+    createdAt,
+    creator
+  }
+  bugService.save(bug)
+    .then(savedBug => {
+      console.log('savedBug', savedBug)
+      res.send(savedBug)
+    })
+})
+
+// Remove bug //TODO - add cookie "nickname"
+app.delete('/api/bug:bugId', (res, req) => {
+  const { bugId } = req.params
+  bugService.remove(bugId)
+    .then(() => {
+      res.send('Deleted!')
+    })
+
+
+  app.listen(port, () => {
+    console.log(`My app listening at http://localhost:${port}`)
+  })
 })
 
