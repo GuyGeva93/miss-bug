19d89ea328d9015af30f912c84457a3a054fcf2b
Guy Geva <guy1030@gmail.com>
2021-06-29 17:42:41 +0300

    service done CRUD work on backend, tested with postman

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-const express = required('express')
-const coockieParser = required('coockie-parser')
+const express = require('express')
+const cookieParser = require('cookie-parser')
 
-const bugService = required('./services/bug-service')
+const bugService = require('./services/bug-service.js')
 const app = express()
 const port = 3000
 
 // Express App Configuration
 // Ask Express.js to serve static files from the 'public' folder
-app.use(express.static('public'))
+// app.use(express.static('public'))
 app.use(cookieParser())
 app.use(express.json())
 
 // Get Bugs list
-app.get('/api/bugs', (res, req) => {
+app.get('/api/bug', (req, res) => {
   bugService.query()
     .then(bugs => res.send(bugs))
 })
 
 // Get Single bug
-app.get('/api/bugs/:bugId', (res, req) => {
+app.get('/api/bug/:bugId', (req, res) => {
   const { bugId } = req.params
   bugService.getById(bugId)
     .then(bug => res.send(bug))
 })
 
 // Create bug
-app.post('/api/bugs', (res, req) => {
-  const { _id, title, description, severity, createdAt, creaotr } = req.body
+app.post('/api/bug', (req, res) => {
+  const { title, description, severity, createdAt, creator } = req.body
   const bug = {
-    _id,
     title,
     description,
     severity,
@@ -43,8 +42,8 @@ app.post('/api/bugs', (res, req) => {
 })
 
 // Edit bug
-app.put('/api/bug', (res, req) => {
-  const { _id, title, description, severity, createdAt, creaotr } = req.body
+app.put('/api/bug', (req, res) => {
+  const { _id, title, description, severity, createdAt, creator } = req.body
   const bug = {
     _id,
     title,
@@ -55,17 +54,20 @@ app.put('/api/bug', (res, req) => {
   }
   bugService.save(bug)
     .then(savedBug => {
-      console.log('savedBug', savedBug)
       res.send(savedBug)
     })
 })
 
 // Remove bug //TODO - add cookie "nickname"
-app.delete('/api/bug:bugId', (res, req) => {
+app.delete('/api/bug/:bugId', (req, res) => {
   const { bugId } = req.params
+  console.log('bugId', bugId)
   bugService.remove(bugId)
     .then(() => {
       res.send('Deleted!')
+    })
+    .catch(err => {
+      console.log(err);
     })
 })
 
--- services/bug-service.js
+++ services/bug-service.js
@@ -1,4 +1,67 @@
 
-export const bugService = {
+const fs = require('fs')
+const gBugs = require('../data/bug.json')
 
+
+module.exports = {
+  query,
+  getById,
+  remove,
+  save,
+}
+
+function query() {
+  return Promise.resolve(gBugs)
+}
+
+function getById(bugId) {
+  const bug = gBugs.find(bug => bug._id === bugId)
+  return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+  const idx = gBugs.findIndex(bug => bug._id === bugId)
+  gBugs.splice(idx, 1)
+  return _saveToFile()
+}
+
+function save(bugToSave) {
+  const { title, description, severity, createdAt, creator } = bugToSave
+  const bug = {
+    _id: bugToSave._id || _makeId(),
+    title,
+    description,
+    severity,
+    createdAt,
+    creator
+  }
+  if (bugToSave._id) {
+    const idx = gBugs.findIndex(bug => bug._id === bugToSave._id)
+    gBugs[idx] = bug
+  }
+  else {
+    gBugs.unshift(bug)
+  }
+  return _saveToFile()
+    .then(() => {
+      return bug
+    })
+}
+
+function _makeId(length = 5) {
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var txt = '';
+  for (let i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return txt;
+}
+
+function _saveToFile() {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('data/bug.json', JSON.stringify(gBugs, null, 2), (err) => {
+      if (err) return reject(err)
+      resolve();
+    })
+  })
 }
