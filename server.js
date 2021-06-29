b9c6e331c84ed39095ef6fed7d7aa0b0d3116f7f
Guy Geva <guy1030@gmail.com>
2021-06-29 14:39:17 +0300

    Working on server.js

--- server.js
+++ server.js
@@ -2,3 +2,25 @@ const express = required('express')
 const coockieParser = required('coockie-parser')
 
 const bugService = required('./services/bug-service')
+const app = express()
+const port = 3000
+
+// Express App Configuration
+// Ask Express.js to serve static files from the 'public' folder
+app.use(express.static('public'))
+app.use(cookieParser())
+app.use(express.json())
+
+// get Bugs list
+app.get('/api/bugs', (res, req) => {
+  bugService.query()
+    .then(bugs => res.send(bugs))
+})
+
+// get Single bug
+app.get('/api/bugs/:bugId', (res, req) => {
+  const { bugId } = req.params
+  bugService.getById(bugId)
+    .then(bug => res.send(bug))
+})
+
